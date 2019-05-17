cda3c5732aa7a8080ba1394b0ade2d0cb5f21b89
Rachel Hollister <rachelhollister@ymail.com>
2019-05-16 20:53:46 -0700

    updated routes on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ClientList from "./pages/ClientList";
 import Menu from "./pages/Menu";
 import OwnerList from './pages/OwnerList';
 import NoMatch from './pages/NoMatch';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/list" component={ClientList} />
-          <Route exact path="/adminlist" component={OwnerList} />
-          <Route exact path="/menu" component={Menu} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/ClientList" component={ClientList} />
+          <Route exact path="/OwnerList" component={OwnerList} />
+          <Route exact path="/Menu" component={Menu} />
           <Route component={NoMatch} />
         </Switch>
         <Footer />
+        <span>
+          <Link to="/">Home</Link>
+          <Link to="/Login">Login</Link>
+          <Link to="/ClientList">ClientList</Link>
+          <Link to="/Menu">Menu</Link>
+          <Link to="/OwnerList">OwnerList</Link>
+        </span>
       </div>
     </Router>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
-  return(
-    <h1>Home</h1>
+  return (
+    <>
+    <div class="jumbotron jumbotron-fluid">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">Angry Fish Sushi</p> 
+    </div>
+    <div class="container-fluid">
+    <img src="images/sushi_img.jpg" alt="strawberry roll" height="600px" width="600px"></img>
+    <p className="owner-login">Welcome to Angry Fish Sushi</p>
+    <Link to="/Menu"><button className="search-button" type="submit">Browse Menu</button>
+    </Link>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    </div>
+    </div>
+    </>
   );
-}
-
+};
 export default Home;
