389a4d534a050d2ca1554d2f4a9930e0da1b1c2c
dstarrtrey <dstarrtrey@gmail.com>
2019-06-04 16:24:43 -0700

    modifies .env req

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const createServer = require('./createServer');
