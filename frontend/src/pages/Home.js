cb8013d6eb46da4f25199a84b251f1dc1c774b8f
Michael <michaelbao13@gmail>
2019-06-04 08:02:19 -0700

    nav and temp menu

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Col, Row, Container } from "../components/Grid/index";
 import Banner from "../components/Banner/index";
 import "./Home.css";
-import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
-
+import NavBar from '../components/NavBar/index'
 
 function Home() {
   return (
     <div className="styledHome">
+      <NavBar>
+      </NavBar>
       <Banner backgroundImage="images/sushibanner.jpg">
         {/* <h1>Angry Fish Sushi</h1>
         <h2>San Leandro, CA</h2> */}
@@ -21,14 +22,16 @@ function Home() {
         </Row>
         <Row>
           <Col className="box" size="md-12 lrg-12">
-          <p><i className="fas fa-map-marker-alt"></i> LOCATION - 16250 E 14th St, San Leandro, CA 94578</p>
+          <p><a style={{ color: '#FFF' }}  href="https://goo.gl/maps/M4qxJFJVno32" target="_blank" ><i className="fas fa-map-marker-alt"></i> LOCATION - 16250 E 14th St, San Leandro, CA 94578</a></p>
           <hr></hr>
-          <p><i className="fas fa-utensils" ></i> TYPE OF CUISINE - Japanese</p>
+          <Link to="/Menu">
+          <p><i className="fas fa-utensils" ></i> TYPE OF CUISINE - Japanese ( Menu )</p>
+          </Link>
           <hr></hr>
-          <p>RATINGS - <i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="far fa-star"></i></p>
+          <p><a style={{ color: '#FFF' }}  href="https://www.yelp.com/biz/angry-fish-sushi-san-leandro" target="_blank" >RATINGS - <i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="far fa-star"></i></a></p>
           <hr></hr>
-          <p><i className="fas fa-clock"></i> AVERAGE WAIT TIME - 30 minutes</p>
-          <Link to="/Menu"><Button type="submit">Browse Menu</Button>
+          <Link to="/ClientList">
+          <p><i className="fas fa-clock"></i> AVERAGE WAIT TIME - 30 minutes ( Wait List )</p>
           </Link>
           </Col>
         </Row>
